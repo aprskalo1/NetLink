f9e19a1799db8d635275af88eaaafdc7f0cb906f
aprskalo <ante.prskalo4@gmail.com>
2024-11-12 12:51:21 +0100

    [NetLink API] - model enduser changed

--- NetLink/Models/EndUser.cs
+++ NetLink/Models/EndUser.cs
@@ -3,10 +3,11 @@
 public class EndUser(string? id)
 {
     public string? Id { get; } = id;
-
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public DateTime CreatedAt { get; init; }
-
     public DateTime? DeletedAt { get; set; }
-
     public bool Active { get; set; }
 }
